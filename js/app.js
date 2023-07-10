8e892d4ac27fb8fd36df30a1fb0afe52b36c5914
Enzo Defranco <enzolobod22@gmail.com>
2023-07-10 17:20:26 -0300

    Agrego media query hasta 501px

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Esta funcion permite abrir y cerrar el menu de navegacion en dispositivos moviles
-function toggleMenu() { 
-  const navMenu = document.querySelector('.nav-menu'); // Selecciona el elemento con la clase nav-menu
-  navMenu.classList.toggle('menu_opened'); // Agrega o remueve la clase menu_opened
+function togglemenu () { 
+  const navmenu = document.querySelector('.menu'); // Selecciona el elemento con la clase menu 
+  navmenu.classList.toggle('menu_opened'); // Agrega o remueve la clase menu _opened
 }
 
-const openMenuBtn = document.querySelector('.open-menu'); // Selecciona el elemento con la clase open-menu
-openMenuBtn.addEventListener('click', toggleMenu); // Agrega el evento click al elemento openMenuBtn y ejecuta la funcion toggleMenu
+const openmenuBtn = document.querySelector('.open-menu'); // Selecciona el elemento con la clase open-menu 
+openmenuBtn.addEventListener('click', togglemenu); // Agrega el evento click al elemento openmenu Btn y ejecuta la funcion togglemenu 
 
-const closeMenuBtn = document.querySelector('.close-menu'); // Selecciona el elemento con la clase close-menu
-closeMenuBtn.addEventListener('click', toggleMenu); // Agrega el evento click al elemento closeMenuBtn y ejecuta la funcion toggleMenu
+const closemenuBtn = document.querySelector('.close-menu'); // Selecciona el elemento con la clase close-menu 
+closemenuBtn.addEventListener('click', togglemenu); // Agrega el evento click al elemento closemenu Btn y ejecuta la funcion togglemenu 
 
