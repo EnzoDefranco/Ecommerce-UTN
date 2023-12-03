f57ad01254bf058e8c77d9b5f9e75c4f4976e808
Enzo Defranco <enzolobod22@gmail.com>
2023-12-03 12:48:53 -0300

    Prueba funcional con todas las rutas testeadas

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./Database');
+const db = require("./Database/db");
+const controllers = require("./controllers");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+/* GET */
+app.get("/products", controllers.getProducts);
+app.get("/products-cart", controllers.getProductsCart);
+
+/* POST */
+app.post("/products-cart", controllers.addProductCart);
+
+/* PUT */
+app.put("/products-cart/:productId", controllers.putProduct);
+
+/* DELETE */
+app.delete("/products-cart/:productId", controllers.deleteProduct);
 
 app.listen(4000, () => {
-  console.log('Server listening on port 5000');
+  console.log("Server funcionando en el puerto 4000");
   db();
 });
 
